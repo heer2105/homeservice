f9f22c5761527e73503f9a18025939ee8786a448
Ubuntu <ubuntu@ip-172-31-7-217.ap-south-1.compute.internal>
2025-03-22 07:33:19 +0000

    update admin.php

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -5,9 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class admin extends Model
+class Admin extends Model
 {
     use HasFactory;
+    
     protected $connection = "mongodb";
     protected $collection = "admins";
+    
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
 }
